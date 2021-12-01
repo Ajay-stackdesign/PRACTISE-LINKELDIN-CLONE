8287fff7d9e6a99cefd40a9bc17cb9bf0d6f3a5d
Ajay-stackdesign <ajaygsahani968@gmail.com>
2021-12-01 23:24:04 +0530

    hnages

--- src/post/Post.js
+++ src/post/Post.js
@@ -7,43 +7,48 @@ import InsertCommentIcon from '@mui/icons-material/InsertComment';
 import ShareIcon from '@mui/icons-material/Share';
 import "./Post.css"
 import { IconButton } from "@mui/material";
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
+// import Header from "./header/Header";
+// import Footer from "./footer/Footer";
 
 
-const Post = () => {
+const Post = ({ username, field, experience, imageURL, description, likes }) => {
     return (
-        <div className="post">
-            <p className="text"><strong>username</strong> likes this</p>
-            <div className="post__avatar">
-                <Avatar alt="Ajay sahani" src="/static/images/avatar/1.jpg" />
-            </div>
-            <div className="post_name">
-                <p>username .tier</p>
-                <p>description</p>
-                <p>experience</p>
-            </div>
-            <p className="post__description">description</p>
-            <img className="post__image" src="https://blogbox.indianeagle.com/wp-content/uploads/2013/10/777-days-of-the-incredible-indian-himalayas-for-adventure-tourists.jpg" alt="himalayan" />
-            <div className="post_flex">
-                <div className="post_likes__shows">
-                    <ThumbUpIcon fontSize="small" />
-                    <FavoriteBorderIcon fontSize="small">number</FavoriteBorderIcon>
+        <div className="main">
+            <Header />
+            <div className="post">
+                <p className="text"><strong>{ username }</strong> likes this</p>
+                <div className="post__avatar">
+                    <Avatar alt="Ajay sahani" src="/static/images/avatar/1.jpg" />
                 </div>
-                <div className="post__text">
-                    <p>no comments</p>
+                <div className="post_name">
+                    <p className="post__tier"><strong>{username}</strong> .tier</p>
+                    <p>{ field }</p>
+                    <p>{ experience }</p>
                 </div>
-            </div>
-            <div className="post__working">
-                <IconButton className="icon__thump">
+                <p className="post__description">{ description }</p>
+                <img className="post__image" src={ imageURL } alt="himalayan" />
+                <div className="post_flex">
+                    <div className="post_likes__shows">
+                        <ThumbUpIcon fontSize="small" />
+                        <FavoriteBorderIcon fontSize="small"></FavoriteBorderIcon>{likes}
+                    </div>
+                    <div className="post__text">
+                        <p>no comments</p>
+                    </div>
+                </div>
+                <div className="post__working">
                     <ThumbUpAltIcon fontSize="large" />
-                </IconButton>
-                <IconButton className="icon__comment">
-                    <InsertCommentIcon fontSize="large" />
-                </IconButton>
-                <IconButton className="icon__share" fontSize="large" >
-                    <ShareIcon />
-                </IconButton>
-                
+                    <IconButton className="icon__comment">
+                        <InsertCommentIcon fontSize="large" />
+                    </IconButton>
+                    <IconButton className="icon__share" fontSize="large" >
+                        <ShareIcon />
+                    </IconButton>
+                </div>
             </div>
+            <Footer />
         </div>
     )
 }
